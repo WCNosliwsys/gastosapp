cbdf22ec2330648e8b253d40f698d371fd30e4b6
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-09 22:06:49 -0500

    init design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,44 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print("presionado");
+                },
+                child: Container(
+                  color: Color(0xff101321),
+                  width: double.infinity,
+                  height: 120,
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(34),
+                      bottomRight: Radius.circular(34),
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 75,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
