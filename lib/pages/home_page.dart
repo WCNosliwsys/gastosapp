b27b99a1248fc9dad4e25ccbdc3e097e9306e571
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-09 22:24:35 -0500

    init deign

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,42 +4,91 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  print("presionado");
-                },
-                child: Container(
-                  color: Color(0xff101321),
-                  width: double.infinity,
-                  height: 120,
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(34),
-                      bottomRight: Radius.circular(34),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("presionado");
+                  },
+                  child: Container(
+                    color: Color(0xff101321),
+                    width: double.infinity,
+                    height: 120,
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(34),
+                        bottomRight: Radius.circular(34),
+                      ),
+                      color: Colors.white,
                     ),
-                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Resumen de gastos",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "Gestiona tus gastos de la mejor forma",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black45),
+                            ),
+                            ListTile(
+                              leading: CircleAvatar(),
+                              title: Text(
+                                "Compras en el super",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              subtitle: Text(
+                                "09/02/2024 22:21",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              trailing: Text(
+                                "S/ 402.00",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 75,
-              ),
-            ],
-          )
-        ],
+                SizedBox(
+                  height: 75,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
